726e57f63581a61c73c13126c8e474f312f8d25f
Sladderdask <dennis.harding2@gmail.com>
2026-05-07 10:39:03 +0200

    Refactor geom_text for conditional label formatting
    Updated geom_text to conditionally format labels with '+' for positive changes and adjusted vertical positioning.

--- R/disease_rate_change.R
+++ R/disease_rate_change.R
@@ -23,11 +23,18 @@ ggplot(df, aes(x = reorder(Disease, Percent_Change),
                y = Percent_Change,
                fill = Percent_Change > 0)) +
   geom_bar(stat = "identity", width = 0.7, alpha = 0.9) +
-  geom_text(aes(label = paste0(round(Percent_Change, 1), "%")),
-            vjust = ifelse(df$Percent_Change > 0, -0.5, 1.5),
-            size = 5) +
+  geom_text(aes(label = ifelse(
+    Percent_Change > 0,
+    paste0("+", round(Percent_Change, 1), "%"),
+    paste0(round(Percent_Change, 1), "%")),
+            vjust = ifelse(Percent_Change > 0, 1, -0.5),
+            size = 6),
+    fontface = "bold",
+    color = "white"
+    ) +
   scale_fill_manual(values = c("TRUE" = "firebrick",
                                "FALSE" = "steelblue")) +
+  ylim(-100,100) +
   labs(
     title = "Percent Change in Deaths per 100,000",
     x = NULL,
@@ -39,3 +46,4 @@ ggplot(df, aes(x = reorder(Disease, Percent_Change),
     axis.text.x = element_text(face = "bold", angle = 0, hjust = 0.5),
     plot.title = element_text(face = "bold")
   )
+
